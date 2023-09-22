39cc10fe74bc85ded4e2b6c8f3b6b21200902f6f
ylsewlys <129646270+ylsewlys@users.noreply.github.com>
2023-09-22 15:51:13 +0800

    color mode

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import App from './App'
-import theme from './Theme'
+import theme from './theme'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
